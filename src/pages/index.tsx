a637dd95a3d53c2f66b36f58a76f4b37cb70fab7
Kalissaac <kalissaac@protonmail.com>
2021-12-13 14:32:19 -0800

    Various updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 export default function HomePage (): JSX.Element {
   return (
-    <div className='flex items-center justify-items-center'>
+    <div className='flex items-center justify-center h-screen'>
       Hello, world!
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/{components,pages}/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/{components,pages}/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
